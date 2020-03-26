4253e04d950f9c3bba1856c7c904b7f9fea46303
Oscar Guzman <18oscarguzman@gmail.com>
2020-03-26 13:09:49 -0500

    API Reflections (not finished)

---
+++ src/main/java/org/basecampcodingacademy/reflections/domain/Reflection.java
@@ -0,0 +1,23 @@
+package org.basecampcodingacademy.reflections.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Reflection {
+
+    public Integer id;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    public LocalDate date;
+    public List<Question> questions;
+
+    public Reflection() {
+    }
+
+    public Reflection(Integer id, LocalDate date, List<Question> questions) {
+        this.id = id;
+        this.date = date;
+        this.questions = questions;
+    }
+}
